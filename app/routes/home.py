b1508f7bd4af3d04eebb47d4edb1eaeb74903f8e
Niccatania <Niccatania6@gmail.com>
2022-12-23 01:15:41 -0600

    add login/signup functionality

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -10,11 +10,15 @@ def index():
   db=get_db()
   #Retrieving all of our post in descending order of creation time
   posts = db.query(Post).order_by(Post.created_at.desc()).all()
-  return render_template('homepage.html', posts=posts)
+  return render_template('homepage.html', posts=posts, loggedIn=session.get('loggedIn'))
 
 @bp.route('/login')
 def login():
-  return render_template('login.html')
+  # not logged in yet
+  if session.get('loggedIn') is None:
+    return render_template('login.html')
+
+  return redirect('/dashboard')
 
 @bp.route('/post/<id>')
 def single(id):
@@ -22,4 +26,4 @@ def single(id):
   db = get_db()
   post = db.query(Post).filter(Post.id == id).one()
   #Rendering our template for displaying a single post
-  return render_template('single-post.html', post=post)
+  return render_template('single-post.html', post=post, loggedIn=session.get('loggedIn'))
